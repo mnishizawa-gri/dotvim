879750daca64ea8c6de10acea7a115338c9f575c
Mike Nishizawa <mnishizawa@thegrandriver.com>
2011-08-22 15:30:18 -0400

    bug fixes for install script

--- install.sh
+++ install.sh
@@ -1,18 +1,25 @@
 
-sudo apt-get install ctags L9
+sudo apt-get install ctags
 
-mv .. ~/.vim
+dirname=`pwd`
+cd ..
+echo "... installing vim directory"
+mv $dirname ~/.vim
 if [ -e ~/.vimrc ]; then 
    echo "You already have a .vimrc specified.  Make sure you have updated the vimrc in this repository with anything you want to retain and delete the one in your home directory."
    exit 1
 fi
 
-exit 0
+echo "... creating symlinks for config files"
 ln -s ~/.vim/vimrc ~/vimrc
 ln -s ~/.vim/gvimrc ~/gvimrc
 
 cd ~/.vim
+echo "... initializing modules"
 git submodule update --init
+
+echo "... configuring pathogen"
 mkdir autoload
 ln -s ~/.vim/bundle/pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
 
+echo "VIM setup complete"
